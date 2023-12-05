51cdd30c84357e0fdbd6831b622c9568e0ae6968
mihailovich <99077545+mihailovichd@users.noreply.github.com>
2023-12-05 20:43:51 +0300

    small fix

--- 004-stream/game/index.js
+++ 004-stream/game/index.js
@@ -8,13 +8,9 @@ rl.question('Напишите имя файла для логирования и
     const playGame = () => {
         rl.question('Загадно число от 1 до 2', (answer) => {
             const number = Math.ceil(Math.random() * 2)
-            if (+answer == number) {
-                console.log('Вы выиграли!')
-                logFile.write('true\n')
-            } else {
-                console.log(`Было загадано число ${number}`)
-                logFile.write('false\n')
-            }
+            const isWon = +answer == number
+            console.log(isWon ? 'Вы выиграли!' : `Было загадано число ${number}`)
+            logFile.write(isWon ? 'true\n' : 'false\n')
             playGame()
         })
     }
