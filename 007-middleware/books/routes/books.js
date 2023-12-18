fbf24205ae6195e52a9056d42e29c7c408978b93
mihailovich <99077545+mihailovichd@users.noreply.github.com>
2023-12-18 22:35:55 +0300

    fixes

--- 007-middleware/books/routes/books.js
+++ 007-middleware/books/routes/books.js
@@ -1,15 +1,37 @@
 const express = require('express')
 const router = express.Router()
 const fileMulter = require('../middleware/files')
-const booksStorage = require('../booksStorage')
 const path = require('path')
+const { v4: uuid } = require('uuid')
+
+class Book {
+    constructor(title = '', description = '', authors = '', favorite = '', fileCover = '', fileName = '', fileBook = '', id= uuid()) {
+        this.id = id
+        this.title = title
+        this.description = description
+        this.authors = authors
+        this.favorite = favorite
+        this.fileCover = fileCover
+        this.fileName = fileName
+        this.fileBook = fileBook
+    }
+}
+
+const booksStorage = []
 
 router.get('/:id/download', (req, res) => {
     const { id } = req.params
-    const { fileBook, fileName } = booksStorage.get(id)
+    const idx = booksStorage.findIndex(el => el.id === id)
+    const { fileBook, fileName } = booksStorage[idx]
     res.download(path.join(__dirname, '../', fileBook), fileName)
 })
 
-router.post('/upload', fileMulter.single('file'), booksStorage.init)
+router.post('/upload', fileMulter.single('file'), (req, res) => {
+    const { title, description, authors, favorite, fileCover, fileName } = req.body
+    const fileBook = req.file.path
+    const book = new Book(title, description, authors, favorite, fileCover, fileName, fileBook)
+    booksStorage.push(book)
+    res.json(book)
+})
 
 module.exports = router
