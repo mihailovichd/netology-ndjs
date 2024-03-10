1edeaf930866574964b65e4a9a5d1144c11cef16
mihailovich <99077545+mihailovichd@users.noreply.github.com>
2024-03-10 14:53:48 +0300

    delete files

--- TypeScript, Nest.js, Yandex Cloud/004-migrate/books/src/inversify.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.container = void 0;
-var services_1 = require("./services");
-var inversify_1 = require("inversify");
-var container = new inversify_1.Container;
-exports.container = container;
-container.bind(services_1.BooksRepository).toSelf();
